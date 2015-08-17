0ad5422fbdd07227ab00c8454cf590f940b2cd7a
elainapolson <elainapolson@gmail.com>
2015-08-17 16:37:39 -0400

    refactor

--- app/models/song.rb
+++ app/models/song.rb
@@ -13,21 +13,16 @@ class Song < ActiveRecord::Base
   end
 
   def sort_to_playlist(user)
-    if self.bpm
-      if self.bpm < 70 
-        self.playlist = Playlist.find_by(:name => "Sleep", :user_id => user.id)
-        self.save
-      elsif self.bpm >= 70 && self.bpm < 90
-        self.playlist = Playlist.find_by(:name => "Study", :user_id => user.id)
-        self.save
-      elsif self.bpm >= 90 && self.bpm < 120  
-        self.playlist = Playlist.find_by(:name => "Party", :user_id => user.id)
-        self.save
-      else 
-        self.playlist = Playlist.find_by(:name => "Workout", :user_id => user.id)
-        self.save
-      end
+    if self.bpm < 70 
+      self.playlist = Playlist.find_by(:name => "Sleep", :user_id => user.id)
+    elsif self.bpm >= 70 && self.bpm < 90
+      self.playlist = Playlist.find_by(:name => "Study", :user_id => user.id)
+    elsif self.bpm >= 90 && self.bpm < 120  
+      self.playlist = Playlist.find_by(:name => "Party", :user_id => user.id)
+    else 
+      self.playlist = Playlist.find_by(:name => "Workout", :user_id => user.id)
     end
+    self.save
   end
 
 end
